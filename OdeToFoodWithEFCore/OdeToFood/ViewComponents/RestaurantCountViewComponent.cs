0d46b9fa40617e47eb7466b5cd406119832e8014
ozhangulal <ozhangulal@outlook.com>
2019-09-03 14:39:36 +0300

    DotNetCoreWithEFCoreFundamentalsProject
    Working with SQL Server and the Entity Framework Core

---
+++ OdeToFoodWithEFCore/OdeToFood/ViewComponents/RestaurantCountViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Data;
+
+namespace OdeToFood.ViewComponents
+{
+    public class RestaurantCountViewComponent:ViewComponent
+    {
+        public IRestaurantData RestaurantData { get; }
+        public RestaurantCountViewComponent(IRestaurantData restaurantData)
+        {
+            RestaurantData = restaurantData;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var count = RestaurantData.GetCountOfRestaurants();
+            return View(count);
+        }
+       
+    }
+}
